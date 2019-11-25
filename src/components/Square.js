a0fd08aa76a48d9cd8b4cfcc25c263f2a681f2ef
Theodore Byron <ted@tedbyron.com>
2019-11-25 15:24:59 -0500

    Update dependencies; replace css with scss

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './Square.css';
 
 /**
  * represents a Square component
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import 'normalize.css';
-import './index.css';
+import './scss/style.scss';
 import Game from './components/Game';
 
 // render the game into the DOM in #root
